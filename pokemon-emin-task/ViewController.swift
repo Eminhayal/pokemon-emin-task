3f255c7be91a52754f0af849927b5d9d47760a3b
Emin Hayal <emin.hayal@yahoo.com>
2022-04-20 18:54:44 +0300

    added

---
+++ pokemon-emin-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokemon-emin-task
+//
+//  Created by Emin Hayal on 20.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
